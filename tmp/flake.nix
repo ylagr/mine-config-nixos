911c767cb2764a0eea8cf442977bfc358210b29d
ssuiwp <suiwp@qq.com>
2025-10-21 08:17:42 +0000

    add tmp/flake.nix.
    Signed-off-by: ssuiwp <suiwp@qq.com>

---
+++ tmp/flake.nix
@@ -0,0 +1,30 @@
+
+{
+  description = "NixOS flake-configuration with Noctalia";
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    quickshell = {
+      url = "github:outfoxxed/quickshell";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    noctalia = {
+      url = "github:noctalia-dev/noctalia-shell";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.quickshell.follows = "quickshell";
+    };
+  }; 
+
+ 
+  outputs = inputs@{ self, nixpkgs, ... }: {
+    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      specialArgs = { inherit inputs; };
+      modules = [
+        ./host/configuration.nix
+       
+       ./noctalia.nix
+       # ./chinese.nix
+      ];
+    }; 
+  };
+}

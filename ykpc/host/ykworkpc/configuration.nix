b5ee08425fb4a3bfdea174e96a84b6c0d65ce18b
ylagr <ylagr@hotmail.com>
2025-12-04 15:00:40 +0800

    system add inetutils

--- ykpc/host/ykworkpc/configuration.nix
+++ ykpc/host/ykworkpc/configuration.nix
@@ -369,6 +369,7 @@ in
     #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     #  wget
     xdg-utils
+    inetutils
     # 光盘刻录
     kdePackages.k3b
     cdrtools
